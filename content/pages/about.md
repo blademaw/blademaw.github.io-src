d27822686046f898095ca853c52bfe466b03cae8
blademaw <jackoli3@me.com>
2021-09-19 22:56:01 +1000

    Init commit

---
+++ content/pages/about.md
@@ -0,0 +1,14 @@
+title: About
+slug: about
+Template: about
+
+<img src="/images/me.jpg" align="left" width="150" style="padding-right:20px;"/>
+Hi! — My name is Jack, and I'm an aspiring data scientist with a passion for computer science. 
+I'm currently completing a Bachelor's degree of Computer Science
+at [Monash University](https://www.monash.edu/). When I'm not occupied with school,
+I enjoy crafting side-projects that give me space to experiment and explore topics in data science
+or computer science. Sometimes I end up uploading them to this blog and talking about them.
+<br clear="left"/>
+
+**About this blog**: <br/>
+    This blog is built under Pelican, with a (slightly tweaked) theme from [Jake VanderPlas](https://github.com/jakevdp/jakevdp.github.io-source).
---
+++ pelicanconf.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Jack Oliver'
+SITENAME = 'Divide & Ponder'
+SITESUBTITLE = 'Vague ventures stumbling through computer science'
+SITEURL = ''
+TIMEZONE = 'Australia/Melbourne'
+DEFAULT_LANG = 'en'
+
+PATH = 'content'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Article URL Settings
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}/index.html'
+DEFAULT_PAGINATION = 10
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Theme
+THEME = './theme/'
+
+MARKUP = ('md', )
+LIQUID_TAGS = ["img", "literal", "video", "include_code", "notebook"]
+LIQUID_CONFIGS = (("IGNORE_FILES", ".ipynb_checkpoints", ""), ("CODE_DIR", "features/code", ""), ("NOTEBOOK_DIR", "features/notebooks", "")
+) #
+
+# from pelican_jupyter import liquid as nb_liquid
+PLUGINS = ['render_math', 'liquid_tags']
+
+ENABLE_MATHJAX = True # testing this
+EXTRA_HEADER = open('_nb_header.html', encoding='utf-8').read()
+
+# Setting up extra pages
+ABOUT_PAGE = '/pages/about.html'
+GITHUB_USERNAME = 'blademaw'
+LINKED_IN = 'https://www.linkedin.com/in/jack-roy-oliver/'
+GOODREADS = 'https://www.goodreads.com/user/show/104565077-jack-oliver'
+AUTHOR_BLOG = 'http://blademaw.github.io'
+AUTHOR_CV = "https://docs.google.com/viewer?url=https://github.com/blademaw/cv/raw/main/resume.pdf"
+
+SHOW_ARCHIVES = True
+SHOW_FEED = False  # Need to address large feeds
+
+STATIC_PATHS = ['images', 'features', 'favicon.ico']
+
+# License
+# LICENSE_URL = "https://github.com/jakevdp/jakevdp.github.io-source/blob/master/LICENSE"
+# LICENSE = "MIT"
